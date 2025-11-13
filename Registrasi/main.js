6ccb681c81a33e27f70ee6ae76cda393354f0bad
yasirqutfi-gif <yasirqutfi@gmail.com>
2025-11-13 13:43:37 +0700

    Delete Registrasi directory

--- Registrasi/main.js
+++
@@ -1,6 +0,0 @@
-var typed = new Typed(".typing", {
-    strings: [" DI PERPUSTAKAAAN NIBAR"],
-    typeSpeed: 100,   // kecepatan mengetik
-    backSpeed: 60,    // kecepatan menghapus
-    loop: true        // supaya berulang terus
-});
