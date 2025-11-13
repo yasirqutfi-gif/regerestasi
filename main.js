93282da40c95bcba1015cc0af56ae33d7caac872
akbaranggaa <>
2025-11-13 13:25:17 +0700

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+var typed = new Typed(".typing", {
+    strings: [" DI PERPUSTAKAAAN NIBAR"],
+    typeSpeed: 100,   // kecepatan mengetik
+    backSpeed: 60,    // kecepatan menghapus
+    loop: true        // supaya berulang terus
+});
